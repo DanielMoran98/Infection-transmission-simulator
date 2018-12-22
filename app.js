b8a0718e04a754e451ba259b7463c892f4e73705
Daniel Moran <danomoran98@gmail.com>
2018-12-22 18:34:42 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,19 +3,10 @@ var path = require('path');
 var app     = express();
 
 
-
-//Store all HTML files in view folder.
-//app.use(express.static(__dirname + '/routes'));
-//Store all JS and CSS in Scripts folder.
 app.use(express.static(__dirname + ''));
 
-//app.set('views', path.join(__dirname + '/views'))
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-
-
-//app.use(express.static(__dirname + 'public'));
-
 
 app.get('/',function(req, res){
   res.render('index.html')
